53a60e95fb8555ec27774702288779ffe6b50b78
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-07 21:19:32 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,8 @@
+print ("Hi! Friends, let's do some calculation")
+print (30 + 40)
+print (56 - 29)
+'''
+print ("Enjoying our blogs?")
+print ("Let's comment out")
+print (60 / 30)
+'''
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print (30 + 40)
+print (40 - 30)
+print (60 / 30)
+print (30 * 40)
