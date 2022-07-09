4b9e73fc815538965fffa7b38fb5cdedaec2a32d
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-09 13:00:52 +0530

    Add files via upload

---
+++ assignment_operator.py
@@ -0,0 +1,30 @@
+'''
+a = 5
+print (a)
+
+a = a + 5
+print (a)
+
+a = a - 2
+print (a)
+
+a = a * 2
+print (a)
+
+a = a / 2
+print (a)
+'''
+#now, here is a shortcut for calculation.
+a = 5
+print (a)
+a += 5
+print(a)
+
+a -= 2
+print (a)
+
+a *= 2
+print (a)
+
+a /= 2
+print (a)
