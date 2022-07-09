7f13adf204084d597138e47f5fafc9ea39f19644
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-09 19:03:50 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+for x in range (1,11):
+    print (x)
+for y in range (1,11):
+    print (2*y)
+
+num = int(input("Enter a num: "))
+for i in range(1,11):
+    print (num*i)
+
+fruits = ["apple", "banana", "grapes"]
+for fruit in fruits:
+    print(fruit)
+helloworld = "hello world"
+for i in helloworld:
+    print(i)
