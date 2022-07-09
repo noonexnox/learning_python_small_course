0599367cfddf9b364006e6e1bb653c7e6d6df43c
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-09 11:27:23 +0530

    Add files via upload

---
+++ logical_operator.py
@@ -0,0 +1,20 @@
+#and, or, not
+'''
+#and
+True + True = True
+True+ False = False
+False + True = False
+False + False = False
+'''
+'''
+#or
+True + True = True
+True+ False = True
+False + True = True
+False + False = False
+'''
+'''
+#not
+True -> False
+False -> True
+'''
