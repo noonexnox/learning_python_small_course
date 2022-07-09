8e4b52bceac115b179841b526faaf44886db2507
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-09 10:14:42 +0530

    Add files via upload

---
+++ arithmatic_operator.py
@@ -0,0 +1,10 @@
+a = 1000
+b = 4
+
+print (a+b) #addition
+print (a-b) #substraction
+print (a*b) #multiplication
+print (a/b) #division
+print (a%b) #modulus
+print (a//b) #floor division
+print (a**b) #Exponentiation
