06e1342e3cf461db02fc76bc53ba666221ad8aff
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-10 10:08:44 +0530

    Add files via upload

---
+++ python_numbers.py
@@ -0,0 +1,20 @@
+#Python numbers
+#int, float, and complex
+a = 5 #int
+b = 5.0 #float
+c = 5 + 3j #complex
+print(a,b,c)
+print (type(a))
+print (type (b))
+print (type (c))
+
+#max() method
+print (max(80, 90, 100))
+print (max(1000, -20,300))
+print (max(30,0,-1))
+print (max(-1, -2,-10))
+print (max (3.0, 3.1, 3.2))
+#min() method
+print (min(80, 80, 100))
+print (min (30,40, 50))
+print (min(-1, -2, -3))
