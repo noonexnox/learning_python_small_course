07a609441e30d0116e5f60705a429316501636c8
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-10 20:37:08 +0530

    Add files via upload

---
+++ python_tuple.py
@@ -0,0 +1,16 @@
+#python tuple
+e_tuple = ()
+e_tuple= (1,2,3)
+print (e_tuple)
+print(e_tuple[0])
+
+#nested tuple
+n_tuple = ("hello",[1,2,3])
+print (n_tuple[0])
+print (n_tuple[1][1])
+print (n_tuple[1][2])
+
+#negative indexing
+e_tuple = (1,2,3)
+print (e_tuple [-1])
+print (e_tuple[-2])
