9760d73754ad4ae2d7947eb71734009c41036369
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-10 08:59:01 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,23 @@
+a = 1
+while a < 6:
+    print(a)
+    a = a + 1
+print("hello world")
+
+'''
+a = 1
+1 < 6
+a = 2
+2 < 6
+a = 3
+3 < 6
+a = 4
+4 < 6
+a = 5
+5 < 5
+'''
+
+a = True
+while a:
+    print("hello world")
+#it'll continue printing hello world.
