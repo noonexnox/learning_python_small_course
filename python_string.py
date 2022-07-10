1c3604809849fe869f34ae66fc9918566ce86d47
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-10 21:15:09 +0530

    Add files via upload

---
+++ python_string.py
@@ -0,0 +1,20 @@
+my_str = "hello"
+print (my_str)
+my_str_multi = """hello
+world
+hello"""
+print (my_str_multi)
+
+my_new = "My name is The Rock"
+print (my_new[0:10]) #this prints from the 0th to the 10th item
+print (my_new[4:10]) #this prints the items from 4th to 10th
+print (my_new[-1]) #this will print from the end
+
+print (my_new[0:10:2]) #it'll print 0th to 10th but in gap of 2
+
+print (my_new[0:]) #everything will be printed from start
+
+#reverse a str
+print (my_new[::-1]) #everything will be printed but in reverse order
+
+print (my_new[0:-2]) #everything will be printed till 2nd last
