f89b56f9d28ac9dc3e20e2f9b093357b75b1b833
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-10 09:57:52 +0530

    Add files via upload

---
+++ break_&_continue.py
@@ -0,0 +1,15 @@
+#break
+print ("break statement")
+a = "hello"
+for x in a:
+    if x == "l":
+        break
+    print(x)
+
+#continue
+print ("continue statement")
+a = "hello"
+for x in a:
+    if x == "l":
+        continue
+    print (x)
