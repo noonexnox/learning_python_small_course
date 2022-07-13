63e3de27124eb88ea1def364765e32b543b58a99
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-13 19:54:01 +0530

    Add files via upload

---
+++ lamdafunction.py
@@ -0,0 +1,18 @@
+from __future__ import division
+
+
+multi = lambda x:x*2
+print (multi (10))
+
+add = lambda x,y:x+y
+print (add (2,55))
+
+division = lambda a,b:a/b
+print (division (30,5))
+
+
+oddeven = lambda num: True if num%2 == 0 else False
+print (oddeven(100))
+
+oddeven = lambda num: True if num%2 == 0 else False
+print (oddeven(41))
