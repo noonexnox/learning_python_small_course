decc18d74de016c82b809e87cc23499a4b6e036f
The Nox <98145096+noonexnox@users.noreply.github.com>
2022-07-13 14:15:49 +0530

    Create types_of_function.py

---
+++ types_of_function.py
@@ -0,0 +1,10 @@
+#types of function
+#1) built-in function- min(),max(),int()
+print(max(80,90,100))
+x = 10
+print (type(x))
+
+#2) user-defined function
+def hello():
+    print("hello world")
+hello()
